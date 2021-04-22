cb4fde8f11682bf17fcf29c03c17ba9bd1d4eed8
Feni954 <anggelinfenny@gmail.com>
2021-04-22 21:14:37 +0700

    ujian MID

---
+++ UsersProject/app/src/main/java/com/antoniusharpen/usersproject/usersproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.febrijuniardi.usersproject.usersproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.febrijuniardi.usersproject.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
